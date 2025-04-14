6bc3143d36eb55ef93b54ad98ca4d33d8f44c576
idobouskila <94786579+IdoBouskila@users.noreply.github.com>
2025-04-14 23:03:30 +0300

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+			parserOptions: {
+				project: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		plugins: {
+			perfectionist,
+		},
+		ignores: ['**/dist/**', '**/node_modules/**', '**/tsconfig.*.json'],
+		rules: {
+			'@typescript-eslint/no-non-null-assertion': 'warn',
+			'@typescript-eslint/no-confusing-void-expression': 'off',
+			'@typescript-eslint/restrict-template-expressions': 'off',
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					order: 'asc',
+					type: 'line-length',
+					groups: [],
+				},
+			],
+		},
+	},
+);
